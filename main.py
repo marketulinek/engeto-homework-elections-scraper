f61e34b1288b9d672e297aedccba66190e5a17bf
Marketa <habranova.marketa@gmail.com>
2022-08-14 09:52:46 +0200

    Defined function scraping basic info of chosen territory

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ author: Marketa
 discord: jsem opozdilec z minuleho behu akademie a pouzivali jsme Slack :-D
 """
 from validation import validate_command_inputs
+from scraper import scrape_data
 import sys
 
 
 validate_command_inputs(sys.argv)
 
-print('The end.')
+data = scrape_data(sys.argv[1])
--- scraper.py
+++ scraper.py
@@ -5,13 +5,46 @@ import re
 
 def get_list_of_available_urls():
 
-    response = get(TARGET_URL)
     available_urls_list = []
-
-    html_doc = BeautifulSoup(response.text, features='html.parser')
+    
+    html_doc = get_html_doc(TARGET_URL)
     urls_list = html_doc.find_all('a', href=re.compile('ps32'))
 
     for url in urls_list:
         available_urls_list.append(url.attrs['href'])
 
     return available_urls_list
+
+def scrape_data(url):
+    
+    data = []
+    tags_tr = get_html_doc(url).find_all('tr')
+    
+    i = 0
+    for tag_tr in tags_tr:
+        tag_td = tag_tr.find('td', {'class': 'cislo'})
+
+        if tag_td:
+            i = i + 1
+
+            tag_a = tag_td.find('a')
+            city_code = tag_a.get_text()
+            city_url = tag_a.attrs['href']
+
+            tag_td = tag_tr.find('td', {'class': 'overflow_name'})
+            city_name = tag_td.get_text()
+
+            data.append({
+                'code': city_code,
+                'location': city_name
+            })
+
+        if i > 10:
+            break
+
+    return data
+
+def get_html_doc(url):
+
+    response = get(url)
+    return BeautifulSoup(response.text, features='html.parser')
